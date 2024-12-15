5b1cca803f3f0f3254ed04d1a07b830321a9c9b8
Anjuino <horoshcilov14@gmail.com>
2024-12-15 22:55:30 +0700

    1
    Изменил структуру проекта

---
+++ esp32WebSocket/MyCommon.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+const char* ssid = "TP-Link_467D";
+const char* password = "66484608";
+
+const char* ADDR = "192.168.0.105";
+const char* URL = "/ws";
+const uint16_t PORT = 8888;
+
+uint64_t Timer = 0;
+
+uint16_t ReconnectInterval = 2000;
---
+++ esp32WebSocket/MySocket.h
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+#include <WebSocketsClient.h>
+
+WebSocketsClient webSocket;
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+      case WStype_CONNECTED:
+        Serial.println("Connected to server");
+        break;
+
+      case WStype_DISCONNECTED:
+        Serial.println("Disconnected from server");
+        break;
+
+      case WStype_TEXT:
+        Serial.printf("Received message: %s\n", payload);
+        break;
+  }
+}
+
+void WebSocketInit () {
+  webSocket.begin(ADDR, PORT, URL);
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(ReconnectInterval);
+}
+
+void WifiInit () {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
+  }
+}
